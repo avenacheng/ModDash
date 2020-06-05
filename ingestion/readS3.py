292601fe087c0c736c03add4519eff1af119a533
Avena Cheng <avenajiajia@berkeley.edu>
2020-06-04 20:45:28 -0700

    convert to JSON

--- ingestion/readS3.py
+++ ingestion/readS3.py
@@ -1,4 +1,14 @@
-import boto3
+# import boto3
+# with open("s3://redditcommentsbz2/RC_2015-01") as f:
+#     for line in f:
+#         j_content = json.loads(line)
+# jsonDF = spark.read.json("s3://redditcommentsbz2/RC_2015-01")
+# jsonDF.printSchema()
+import json
 
-jsonDF = spark.read.json("s3://redditcommentsbz2/RC_2015-01")
-jsonDF.printSchema()
+with open("RC_2015-01") as f:
+    for line in f:
+        j_content = json.loads(line)
+
+with open('j_content.json', 'w') as outfile:
+    json.dump(j_content, outfile)
