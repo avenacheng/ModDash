b50c3121e68d0d424830455d21ce808501412d67
Avena Cheng <avenajiajia@berkeley.edu>
2020-06-08 14:24:30 -0700

    converting ndjson to parquet

--- ingestion/readS3.py
+++ ingestion/readS3.py
@@ -1,23 +1,13 @@
-# import boto3
-# with open("s3://redditcommentsbz2/RC_2015-01") as f:
-#     for line in f:
-#         j_content = json.loads(line)
-# jsonDF = spark.read.json("s3://redditcommentsbz2/RC_2015-01")
-# jsonDF.printSchema()
 from pyspark.sql import SparkSession
-from pyspark.sql.types import ArrayType, StructField, StructType, StringType, IntegerType
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext
 
 # create Spark context
 sc = SparkContext("spark://ec2-100-21-167-148.us-west-2.compute.amazonaws.com:7077","bz2 to JSON")
+sqlContext = SQLContext(sc)
 
 # read data
-file_path = 's3a://redditcommentsbz2/RC_2014-01.bz2'
+df = sqlContext.read.json(file_path).select("created_utc", "subreddit","subreddit_id",\
+"author","id","body","ups","downs","controversiality","edited","parent_id","link_id")
 
-def read_fun_generator(filename):
-    with bz2.open(filename, 'rb') as f:
-        for line in f:
-            yield line.strip()
-
-
-bz2_filelist = sc.json(file_path)
-rdd_from_bz2 = sc.flatMap(read_fun_generator)
+df.write.json('s3a://redditcommentsbz2/parquet/practice.json')
