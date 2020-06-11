fa4a55e3b580a5d3b504d3135b1335e928916587
Ubuntu <ubuntu@ip-10-0-0-9.us-west-2.compute.internal>
2020-06-11 21:56:44 +0000

    almost complete

--- ingestion/readS3.py
+++ ingestion/readS3.py
@@ -2,9 +2,13 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import from_unixtime
+from pyspark.sql.functions import hour
+from pyspark.sql.functions import dayofmonth
+from pyspark.sql.functions import year
+from pyspark.sql.functions import month
+from pyspark.sql.functions import col
 
-
-file_name = 'RC_2014-02'
+file_name = 'RC_2005-12.bz2'
 file_path = "s3a://redditcommentsbz2/" + file_name
 
 # create Spark context
@@ -13,13 +17,17 @@ sqlContext = SQLContext(sc)
 
 # read in JSON
 df = sqlContext.read.json(file_path).select("created_utc", "subreddit","subreddit_id",\
-"id","name","body","ups","score","author","controversiality","edited","parent_id","link_id")
+"id","body","score","author","controversiality","link_id").withColumnRenamed('created_utc', 'time').na.fill('null')
+# convert UNIX
+#df.show()
+df1 = df.withColumn('time', from_unixtime(df.time, format='yyyy-MM-dd HH:mm:ss'))
+df2 = df1.withColumn('year', year(df1.time))\
+        .withColumn('month', month(df1.time)) \
+        .withColumn('day', dayofmonth(df1.time)) \
+        .withColumn('hour', hour(df1.time))
 
-# convert UNIX 
-convertedUnix = df.withColumn('created_utc', from_unixtime(df.created_utc, format='yyyy-MM-dd HH:mm:ss'))
 
-convertedUnix.show()
 # save to S3
 parquet_name = file_name + '.parquet'
 saved_path = "s3a://redditcommentsbz2/" + parquet_name
-convertedUnix.write.parquet(saved_path)
+df2.write.parquet(saved_path)
