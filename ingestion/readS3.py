2d39a63a6b417e4505e37c0c30c8a97095512340
Ubuntu <ubuntu@ip-10-0-0-9.us-west-2.compute.internal>
2020-06-11 00:25:48 +0000

    hi

--- ingestion/readS3.py
+++ ingestion/readS3.py
@@ -1,24 +1,25 @@
 from pyspark.sql import SparkSession
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import from_unixtime
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = 'file paths')
-    parser.add_argument('--input_path', required = True)
-    parser.add_argument('--output_path', required = True)
-    parser.add_argument('--filename', required = True)
-    args = parser.parse_args()
 
-    # create Spark context
-    sc = SparkContext("spark://ec2-100-21-167-148.us-west-2.compute.amazonaws.com:7077","sparkJob1")
-    sqlContext = SQLContext(sc)
+file_name = 'RC_2014-02'
+file_path = "s3a://redditcommentsbz2/" + file_name
 
-    # read data
-    # example input_path: "s3a://redditcommentsbz2/RC-2015-01"
-    df = sqlContext.read.json(args.input_path).select("created_utc", "subreddit","subreddit_id",\
-    "author","id","body","ups","downs","controversiality","edited","parent_id","link_id")
-    df = df.withColumn('created_utc', from_unixtime(df.created_utc, format='yyyy-MM-dd HH:mm:ss'))
-    parquet_name = args.filename + str('.parquet')
-    final_output_path = args.output_path + parquet_name
-    df.write.parquet(final_output_path)
+# create Spark context
+sc = SparkContext("spark://ec2-100-21-167-148.us-west-2.compute.amazonaws.com:7077","job1")
+sqlContext = SQLContext(sc)
+
+# read in JSON
+df = sqlContext.read.json(file_path).select("created_utc", "subreddit","subreddit_id",\
+"id","name","body","ups","score","author","controversiality","edited","parent_id","link_id")
+
+# convert UNIX 
+convertedUnix = df.withColumn('created_utc', from_unixtime(df.created_utc, format='yyyy-MM-dd HH:mm:ss'))
+
+convertedUnix.show()
+# save to S3
+parquet_name = file_name + '.parquet'
+saved_path = "s3a://redditcommentsbz2/" + parquet_name
+convertedUnix.write.parquet(saved_path)
