969b9149b5683048b3923ad692f4c9bbcc3a0bb4
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2020-06-23 17:01:25 +0000

    hid passwords

--- app/app.py
+++ app/app.py
@@ -8,12 +8,19 @@ from dash.exceptions import PreventUpdate
 import psycopg2
 import pandas as pd
 import pandas.io.sql as sqlio
+import plotly.express as px
 import plotly.graph_objs as go
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+db_user = os.getenv("db_user")
+db_password = os.getenv("db_password")
+UI_host = os.getenv("UI_host")
 
 # connect to database
 try:
-    conn = psycopg2.connect(dbname='reddit', user='postgres', password='hello', host='10.0.0.10')
+    conn = psycopg2.connect(dbname='reddit', user=db_user, password=db_password, host='10.0.0.10')
 except:
     print "I am unable to connect to the database."
 
@@ -21,15 +28,6 @@ except:
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash('ModDash', external_stylesheets = external_stylesheets)
 server = app.server
-
-
-colors = {
-'background': '#07080a',
-'text' : '#d6d8da',
-'plotbg' : '#c6c8ca',
-'gridline' : '#b6b8ba',
-'headertext' : '#df6e22'
-}
 
 app.layout = html.Div([
 
@@ -81,7 +79,7 @@ app.layout = html.Div([
     style_header={
         'backgroundColor': 'white',
         'fontWeight': 'bold'
-    }
+    	}
     ),
 
     html.H4('TOP POSTS',style={'font-family':'arial','font-weight':'bold'}),
@@ -95,19 +93,45 @@ app.layout = html.Div([
     style_header={
         'backgroundColor': 'white',
         'fontWeight': 'bold'
-    },
+   	 },
     style_cell={'textAlign':'left','font-family':'sans-serif'},
     ),
 
 
     html.H4('USER HISTORY',style={'font-family':'arial','font-weight':'bold'}),
-    dcc.Graph(id='user-history')
+    dcc.Input(id='user', placeholder='Enter author here', type='text'),
+    html.Div([
+    html.Button('View User',n_clicks=0, style={'margin-top':'10px'}, id='user-button')
+		]),
+   
+    dcc.Graph(id='user-graph'),
+   
+    dash_table.DataTable(
+    id='user-comments',
+    columns=[{"name": i, "id": i} for i in ['time','author','sentiment','score','comment']],
+    style_cell_conditional=[{'if':{'column_id':'time'},'width':'50px', 'textAlign':'left'},\
+                            {'if':{'column_id':'author'}, 'width':'10px', 'textAlign':'left'},
+                            {'if':{'column_id':'sentiment'}, 'width':'10px', 'textAlign':'left'},
+                            {'if':{'column_id':'score'}, 'width':'10px', 'textAlign':'left'},
+                            {'if':{'column_id':'comment'},'overflowX':'scroll', 'textAlign':'left','height':'auto'},
+
+                            ],
+    style_header={
+        'backgroundColor': 'white',
+        'fontWeight': 'bold'
+         },
+    style_cell={'textAlign':'left','font-family':'sans-serif'},
+    ),
+
+
 ], style={'width':'100%','display':'inline-block'})
 
 
 
 @app.callback(Output('comments','data'),[Input('submit','n_clicks')], [State('year','value'),State('month','value')])
 def update_comments(n_clicks, year, month):
+    if n_clicks is None:
+	raise PreventUpdate
     if n_clicks > 0:
 	select = "SELECT time,post_id,author,score,sentiment,comment FROM comments "
         year = "WHERE year = {} AND ".format(year)
@@ -120,6 +144,8 @@ def update_comments(n_clicks, year, month):
 
 @app.callback(Output('posts','data'),[Input('submit','n_clicks')], [State('year','value'),State('month','value')])
 def update_posts(n_clicks, year, month):
+    if n_clicks is None:
+	raise PreventUpdate
     if n_clicks > 0:
         select = "SELECT post_id,AVG(sentiment) AS avg_sentiment, COUNT(comment) AS total_comments FROM comments "
         year = "WHERE year = {} AND ".format(year)
@@ -131,7 +157,23 @@ def update_posts(n_clicks, year, month):
         df = sqlio.read_sql_query(sql, conn)
         return df.to_dict('records')
 
+@app.callback([Output('user-graph','figure'),Output('user-comments','data')],[Input('user-button','n_clicks')], [State('user','value'),State('year','value'),State('month','value')])
+def update_user(n_clicks, user, year, month):
+    if n_clicks is None or user is None:
+	raise PreventUpdate
+    elif n_clicks > 0:
+	select = "SELECT time, author, sentiment, score, comment FROM comments "
+	year = "WHERE year = {} AND ".format(year)
+	month = "month = {} AND ".format(month)
+	subreddit = "subreddit = 'politics' AND "
+	author_str = "'" + user + "'" 
+	author = " author = {} ".format(author_str)
+	sql = select + year + month + subreddit + author
+	df = sqlio.read_sql_query(sql, conn).sort_values('time')
+	fig = {'data': [go.Scatter(x=df.time, y=df.sentiment,name='Hi')], 'layout': go.Layout(title='User Sentiment')}
+	return fig, df.to_dict('records')
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050, host='ec2-52-88-201-86.us-west-2.compute.amazonaws.com')
+    app.run_server(debug=True, port=8050, host=UI_host)
 
