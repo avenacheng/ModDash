bc7a4cf8532650c23d93a97f2186c06adc29e271
Ubuntu <ubuntu@ip-10-0-0-9.us-west-2.compute.internal>
2020-06-23 18:51:52 +0000

    hid credentials

--- ingestion/readS3.py
+++ ingestion/readS3.py
@@ -1,20 +1,21 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
+from dotenv import load_dotenv
+load_dotenv()
 
-# create Spark context
-sc = SparkContext("spark://ec2-100-21-167-148.us-west-2.compute.amazonaws.com:7077","ingestion")
+file_name = 'RC_2005-12.bz2'
+file_path = "s3a://redditcommentsbz2/" + file_name
+
+# CREATE SPARK CONTEXT
+master = os.getenv('master_host')
+sc = SparkContext(master, "ingestion")
 sqlContext = SQLContext(sc)
 
+# READ IN DATA
+df = sqlContext.read.json(file_path).select("created_utc", "subreddit","subreddit_id",\
+"id","body","score","author","controversiality","link_id")
 
-for year in range(2013,2018):
-    for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:    
-        file_name = 'RC_'+str(year)+'-'+ month
-        file_path = "s3a://redditcommentsbz2/" + file_name + '.bz2'
-
-        df = sqlContext.read.json(file_path).select("created_utc", "subreddit","subreddit_id","id","body","score","author","controversiality","link_id")
-
-        parquet_name = file_name + '.parquet'
-        saved_path = "s3a://redditcommentsbz2/" + parquet_name
-        df.write.parquet(saved_path)
+# SAVE TO S3
+saved_path = "s3a://redditcommentsbz2/" + parquet_name
 
 
